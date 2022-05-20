ec0f22571f68f0dfb1ae9974a93c950b0d898d0f
Lilbunnifoofoo <104391386+Lilbunnifoofoo@users.noreply.github.com>
2022-05-20 11:00:55 -0400

    migration & dbcontext adjust

--- GroupProjLib/Models/GpDbContext.cs
+++ GroupProjLib/Models/GpDbContext.cs
@@ -9,8 +9,8 @@ namespace GroupProjLib.Models {
     public class GpDbContext : DbContext{
 
         public virtual DbSet<Project> Projects { get; set; } = null!;
-        //public virtual DbSet<Resources> Resources { get; set; } = null!;
-        //public virtual DbSet<Work> Works { get; set; } = null;
+        public virtual DbSet<Resources> Resources { get; set; } = null!;
+        public virtual DbSet<Work> Works { get; set; } = null!;
 
         public GpDbContext () { }
         public GpDbContext (DbContextOptions<GpDbContext> options) : base(options) { }
