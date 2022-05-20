75afd24b0a6eff48d8b29e7a426d14c44e698522
Lilbunnifoofoo <104391386+Lilbunnifoofoo@users.noreply.github.com>
2022-05-20 13:52:29 -0400

    made columns nullable, fixed resourceid issue

--- GroupProjLib/Migrations/20220520175149_adjusting errors.Designer.cs
+++ GroupProjLib/Migrations/20220520175149_adjusting errors.Designer.cs
@@ -4,6 +4,7 @@ using GroupProjLib.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GroupProjLib.Migrations
 {
     [DbContext(typeof(GpDbContext))]
-    partial class GpDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220520175149_adjusting errors")]
+    partial class adjustingerrors
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- GroupProjLib/Models/Resources.cs
+++ GroupProjLib/Models/Resources.cs
@@ -12,7 +12,7 @@ namespace GroupProjLib.Models
     public class Resources
     {
         public int Id { get; set; }
-        public int ProjectId { get; set; } 
+        public int? ProjectId { get; set; } 
         public virtual Project Project { get; set; } = null!;
         [StringLength(30)]
         public string Name { get; set; } = null!;
